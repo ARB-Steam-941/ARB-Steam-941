d0a186de129722dd36ee4682b1521eaa93c29fc2
ARB-Steam-941 Labs <159860276+ARB-Steam-941@users.noreply.github.com>
2024-02-14 12:52:04 +0800

    Add files via upload

---
+++ character.sol
@@ -0,0 +1,22 @@
+{
+  "name": "CHARACTER #1",
+  
+    {
+      "trait_type": "",
+      "value": "Archer"
+    },
+    {
+      "trait_type": "",
+      "value": "Hammer"
+    },
+    {
+      "trait_type": "",
+      "value": "Plate"
+    },
+    {
+      "trait_type": "",
+      "value": "Fireball"
+    }
+  ],
+  "compiler": "Steam941"
+}
